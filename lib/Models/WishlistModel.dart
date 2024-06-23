5a65cb3c3937236b0e279d40c88d0db5c1c86a54
Yusra48 <154058242+Yusra48@users.noreply.github.com>
2024-06-24 01:36:06 +0500

    Update WishlistModel.dart

--- lib/Models/WishlistModel.dart
+++ lib/Models/WishlistModel.dart
@@ -1,32 +1,48 @@
-import 'package:stacked/stacked.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
-class WishlistViewModel extends BaseViewModel {
-  List<Map<String, dynamic>> _wishlistItems = [];
+class WishlistViewModel extends ChangeNotifier {
+  List<Map<String, dynamic>> _wishlist = [];
 
-  List<Map<String, dynamic>> get wishlistItems => _wishlistItems;
+  List<Map<String, dynamic>> get wishlist => _wishlist;
 
-  void addToWishlist(Map<String, dynamic> item) {
-    _wishlistItems.add(item);
+  WishlistViewModel() {
+    _loadWishlist();
+  }
+
+  Future<void> _loadWishlist() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? wishlistString = prefs.getString('wishlist');
+    if (wishlistString != null) {
+      _wishlist = List<Map<String, dynamic>>.from(json.decode(wishlistString));
+      notifyListeners();
+    }
+  }
+
+  void addToWishlist(Map<String, dynamic> product) async {
+    _wishlist.add(product);
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('wishlist', json.encode(_wishlist));
     notifyListeners();
+  }
 
-    fetchWishlistItems();
+  void removeFromWishlist(int index) async {
+    _wishlist.removeAt(index);
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('wishlist', json.encode(_wishlist));
+    notifyListeners();
   }
 
   double get totalAmount {
-    return wishlistItems.fold(
+    return _wishlist.fold(
         0, (sum, item) => sum + (double.tryParse(item['price']) ?? 0));
   }
 
   double get totalConvenienceFee {
-    return wishlistItems.fold(
+    return _wishlist.fold(
         0,
         (sum, item) =>
             sum + (double.tryParse(item['convenienceFee'] ?? '0') ?? 0));
-  }
-
-  void fetchWishlistItems() {
-    _wishlistItems = [];
-
-    notifyListeners();
   }
 }
